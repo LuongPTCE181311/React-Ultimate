71dfd5ce084deca1819184528f26a0ac6d2396a8
LuongPTCE181311 <pluongp123@gmail.com>
2025-12-26 14:00:30 +0700

    video 22

--- .history/src/App_20251226134724.jsx
+++ .history/src/App_20251226134724.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <button onClick={() => setCount((count) => count - 1)}>
-          tru count {count}
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
